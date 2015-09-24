5ea503748d9ae59dae15b7619478338227a31222
Ivan Nikolić <ivan.nikolic@trikoder.net>
2015-09-24 10:59:35 +0200

    Make CommonJS compatible
    - whitespace fixes

--- ios-orientationchange-fix.js
+++ ios-orientationchange-fix.js
@@ -4,11 +4,11 @@
 */
 
 (function(w){
-	
+
 	// This fix addresses an Mobile Safari iOS bug, so return early if the UA claims it's something else.
 	var ua = navigator.userAgent;
-	if( !( /iPhone|iPad|iPod/.test( navigator.platform ) 
-            && /OS [1-5]_[0-9_]* like Mac OS X/i.test(ua) 
+	if( !( /iPhone|iPad|iPod/.test( navigator.platform )
+            && /OS [1-5]_[0-9_]* like Mac OS X/i.test(ua)
             && ua.indexOf( "AppleWebKit" ) > -1
             && ua.indexOf( "CriOS") == -1  // chrome for iOS doesn't have the bug
             )){
@@ -34,26 +34,26 @@
         meta.setAttribute( "content", disabledZoom );
         enabled = false;
     }
-	
+
     function checkTilt( e ){
         var ori = w.orientation;
         // if it's landscape we're out of here
         if(90 == Math.abs(w.orientation)) {
             if(enabled)restoreZoom();
             return;
-        } 
-        
+        }
+
 	aig = e.accelerationIncludingGravity;
 	x = Math.abs( aig.x );
 	y = Math.abs( aig.y );
-	
+
         // If in the danger zone where x is much greater than y turn off zoom
         if(y == 0 || (x/y) > 1.2){
             if(enabled)disableZoom();
         }else if( !enabled )restoreZoom();
     }
-	
+
     w.addEventListener( "orientationchange", restoreZoom, false );
     w.addEventListener( "devicemotion", checkTilt, false );
 
-})( this );    
+})( window );
